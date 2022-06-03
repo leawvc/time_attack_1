8920490693b66bbeef24ea97c1b18fdcd71aa5d5
eoeo777755@naver.com <eoeo777755@naver.com>
2022-06-03 20:01:51 +0900

    bulid : timeAtteck.... 실패

---
+++ java0603/NATGateWay.java
@@ -0,0 +1,18 @@
+package java0603;
+
+import jdk.jfr.SettingDefinition;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+public class NATGateWay {
+    private final Long id;
+    private final String subnet;
+
+    void String send(id){
+
+    }
+}
